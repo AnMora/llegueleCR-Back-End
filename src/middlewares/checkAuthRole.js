189d09b99cfa762e9ab9641f49326dd904ef7da0
X <anmora.2396@gmail.com>
2024-11-15 03:41:40 -0600

    save project

--- src/middlewares/checkAuthRole.js
+++ src/middlewares/checkAuthRole.js
@@ -8,7 +8,7 @@ module.exports.checkAuthRole = (roles) => async (req, res, next) => {
       const token = req.headers.authorization.split(" ").pop();
       const tokenData = await UsersService.auth(token);
       const userData = await Users.findById(tokenData._id);
-      //   console.log(userData);
+        console.log(userData);
       if ([].concat(roles).includes(userData.role)) {
         next();
       } else {
@@ -21,3 +21,7 @@ module.exports.checkAuthRole = (roles) => async (req, res, next) => {
     Response.fail(res);
   }
 };
+
+// FIJARSE BIEN YA QUE ESTA VALIDANDO EL ROLE CON LA BASE DE DATOS DE LOS USERS, TIENE QUE SER CON LA DE ADMINS
+// SOLO ADMINS PUEDEN VER REGISTROS DE CLIENTES
+// CAMBIAR CONDICIONES PARA REVISAR POR PERMISO DADO POR UN ADMIN
